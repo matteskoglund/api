ce82aefa9e243512036ae2819984cbe795ddbd26
Mats Skoglund <mats.skoglund@gmail.com>
2016-10-01 18:07:31 +0200

    Snyggat till ett test

--- test/ValuesControllersTest.cs
+++ test/ValuesControllersTest.cs
@@ -15,18 +15,8 @@ namespace test
 
         public void PassingTest()
         {
-            Assert.Equal(4, Add(2, 2));
+            Assert.NotNull(_valuesController);
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
-        int Add(int x, int y)
-        {
-            return x + y;
-        }
     }
 }
