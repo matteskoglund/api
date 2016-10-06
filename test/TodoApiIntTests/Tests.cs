ea5c91521fd690295cba62f8d306f8301c52e067
Mats Skoglund <mats.skoglund@gmail.com>
2016-10-06 20:46:13 +0200

    Skapat integrationstester

---
+++ test/TodoApiIntTests/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TodoApi;
+
+namespace Tests
+{
+
+
+    public class Tests
+    {
+
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public Tests()
+        {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder()
+            .UseStartup<Startup>());
+            _client = _server.CreateClient();
+
+        }   
+
+        [Fact]
+        public async Task ReturnHelloWorld()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/values");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal("[\"Matte\",\"Skoglund\"]",
+            responseString);
+        }
+
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+    }
+}
