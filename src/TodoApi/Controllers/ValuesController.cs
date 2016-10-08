9c058f747eab26693877216be11e2a2d043c402d
Mats Skoglund <mats.skoglund@gmail.com>
2016-10-08 17:56:53 +0200

    Ändrat värde igen

--- src/TodoApi/Controllers/ValuesController.cs
+++ src/TodoApi/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace TodoApi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Mats", "Skoglund" };
+            return new string[] { "Matte", "Skoglund" };
         }
 
         // GET api/values/5
