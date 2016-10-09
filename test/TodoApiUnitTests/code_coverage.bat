c6276802a52ef3805b85d4c0d4583f29b0beae2f
Mats Skoglund <mats.skoglund@gmail.com>
2016-10-09 17:53:59 +0200

    Added support for coverage report

--- test/TodoApiUnitTests/code_coverage.bat
+++ test/TodoApiUnitTests/code_coverage.bat
@@ -15,7 +15,7 @@ REM Run code coverage analysis
 
 %converter% -input:Coverage.xml -output:Cobertura.xml
 REM Generate the report  
-REM %reportgenerator% -targetdir:%coveragedir% -reporttypes:Html;Badges -reports:%coveragefile% -verbosity:Error
+%reportgenerator% -targetdir:%coveragedir% -reporttypes:Html;Badges -reports:%coveragefile% -verbosity:Error
 
 REM Open the report  
 REM start "report" "%coveragedir%\index.htm"  
