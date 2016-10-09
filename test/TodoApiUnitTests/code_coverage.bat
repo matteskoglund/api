5883f41ddc33d4c2464137981403f3d43caa2bdc
Mats Skoglund <mats.skoglund@gmail.com>
2016-10-09 17:27:46 +0200

    Added code coverage

---
+++ test/TodoApiUnitTests/code_coverage.bat
@@ -0,0 +1,21 @@
+@echo off
+
+SET dotnet="C:/Program Files/dotnet/dotnet.exe"  
+SET opencover=C:\Users\mats\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe
+SET reportgenerator=C:\Users\mats\.nuget\packages\ReportGenerator\2.4.5\tools\ReportGenerator.exe
+SET converter=C:\Users\mats\.nuget\packages\OpenCoverToCoberturaConverter\0.2.4\tools\OpenCoverToCoberturaConverter.exe
+SET targetargs="test --no-build -xml testresults.xml"  
+SET filter="+[*]Temperature* -[*.Test]* -[xunit.*]* -[FluentValidation]*"  
+SET coveragefile=Coverage.xml  
+SET coveragedir=Coverage
+
+REM Run code coverage analysis  
+%opencover% -oldStyle -register:user -target:%dotnet% -output:%coveragefile% -targetargs:%targetargs% -filter:%filter% -skipautoprops -hideskipped:All
+
+
+%converter% -input:Coverage.xml -output:Cobertura.xml
+REM Generate the report  
+REM %reportgenerator% -targetdir:%coveragedir% -reporttypes:Html;Badges -reports:%coveragefile% -verbosity:Error
+
+REM Open the report  
+REM start "report" "%coveragedir%\index.htm"  
