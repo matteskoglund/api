26c0de649e5a7541f200846a4b226eeb7b0a54ec
matsskoglund <mats.skoglund@gmail.com>
2016-10-10 10:53:26 +0200

    Update code_coverage.bat

--- test/TodoApiUnitTests/code_coverage.bat
+++ test/TodoApiUnitTests/code_coverage.bat
@@ -4,8 +4,8 @@ SET dotnet="C:/Program Files/dotnet/dotnet.exe"
 SET opencover=C:\Users\mats\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe
 SET reportgenerator=C:\Users\mats\.nuget\packages\ReportGenerator\2.4.5\tools\ReportGenerator.exe
 SET converter=C:\Users\mats\.nuget\packages\OpenCoverToCoberturaConverter\0.2.4\tools\OpenCoverToCoberturaConverter.exe
-SET targetargs="test"  
-SET filter="+[*]* -[*Test]* -[xunit.*]* -[FluentValidation]*"  
+SET targetargs="test --no-build -xml testreport.xml"  
+SET filter="+[*]* -[*Test*]* -[xunit.*]* -[FluentValidation]*"  
 SET coveragefile=Coverage.xml  
 SET coveragedir=Coverage
 
