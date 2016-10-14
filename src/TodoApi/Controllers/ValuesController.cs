70acc59fe2b75014a22c0122cfae36736a085a91
Mats Skoglund <mats.skoglund@scania.com>
2016-10-14 12:41:25 +0200

    Change values again

--- src/TodoApi/Controllers/ValuesController.cs
+++ src/TodoApi/Controllers/ValuesController.cs
@@ -17,7 +17,7 @@ namespace TodoApi.Controllers
         public IEnumerable<string> Get()
         {
 
-            return new string[] { "Matte", "Skoglund" };
+            return new string[] { "Mats", "Skoglund" };
 
         }
 
